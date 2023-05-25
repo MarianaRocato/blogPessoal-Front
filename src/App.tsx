f89bcaa1f64d2d226be9940dabe9a88cf6595a19
Mariana Rocato <mariana.rocato_flaibam@hotmail.com>
2023-05-25 19:46:22 -0300

    atualização de cadastro, inclusão do Service e Model]

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './paginas/home/Home';
 import Navbar from './components/statics/navbar/Navbar';
 import Footer from './components/statics/footer/Footer';
 import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
 function App() {
 
@@ -13,9 +14,10 @@ function App() {
         <Navbar />
         <div style={{ minHeight: '100vh'}}>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Login />} />
             <Route path="/home" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/cadastrousuario" element={<CadastroUsuario />} />
 
           </Routes>
         </div>
